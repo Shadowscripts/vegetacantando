cff7a10611b8175cc178a0181564b01b7bc47e55
Shadowscripts <55377861+Shadowscripts@users.noreply.github.com>
2023-11-16 13:33:43 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <>
     <div id="wrapper">
  <div id="iphone">
   <div id="camera">
@@ -154,6 +154,6 @@ export default function Home() {
   <div id="home-btn"></div>
  </div>
 </div>
-    </main>
+    </>
   )
 }
