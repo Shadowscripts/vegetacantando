b729958117757444d0c17c15bdfc35ea36909f8f
Shadowscripts <55377861+Shadowscripts@users.noreply.github.com>
2023-11-16 13:32:50 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,111 +3,157 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+    <div id="wrapper">
+ <div id="iphone">
+  <div id="camera">
+   <span></span>
+   <span></span>
+  </div>
+  <div id="screen">
+   <div id="content-wrap">
+    <div id="background"></div>
+    <div id="content">
+     <div id="header">
+      <span id="menu-btn"><img src="https://emilcarlsson.se/assets/svg/menu.svg" alt="" class="svg"></span>
+      <span id="options-btn"><img src="https://emilcarlsson.se/assets/svg/more.svg" alt="" class="svg"></span>
+     </div>
+     <div class="jcarousel">
+      <ul id="songs">
+      	<li class="song" data-audio="https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/Leo%20-%20Trying.mp3" data-color="#f38578">
+					<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/trying-album-cover.jpg">
+					<p class="song-title">Trying</p>
+					<p class="song-artist">Leo</p>
+				</li>
+      </ul>
+     </div>
+     <audio crossorigin>
+				<source src="" type="audio/mpeg">
+			</audio>
+     <div id="controls">
+      <span id="previous-btn"><i class="fa fa-step-backward fa-fw" aria-hidden="true"></i></span>
+      <span id="play-btn"><i class="fa fa-play fa-fw" aria-hidden="true"></i></span>
+      <span id="next-btn"><i class="fa fa-step-forward fa-fw" aria-hidden="true"></i></span>
+     </div>
+     <div id="timeline">
+      <span id="current-time">--:--</span>
+      <span id="total-time">--:--</span>
+      <div class="slider" data-direction="horizontal">
+				<div class="progress">
+					<div class="pin" id="progress-pin" data-method="rewind"></div>
+				</div>
+			</div>
+     </div>
+     <div id="sub-controls">
+      <i class="fa fa-random" aria-hidden="true"></i>
+      <i class="fa fa-refresh" aria-hidden="true"></i>
+      <i class="fa fa-bluetooth-b active" id="bluetooth-btn" aria-hidden="true"></i>
+      <i class="fa fa-heart-o" id="heart-icon" aria-hidden="true"></i>
+     </div>
+    </div>
+    <div id="overlay"></div>
+   </div>
+   <div id="sidemenu">
+    <ul>
+     <li>
+      <i class="fa fa-search fa-fw" aria-hidden="true"></i>
+			 Search
+     </li>
+     <li>
+      <img class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/music-player.svg" alt="">
+			 Playlists
+     </li>
+     <li>
+      <img id="album-icon" class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/album-icon.svg" alt="">
+      Albums
+     </li>
+     <li>
+      <i class="fa fa-microphone fa-fw" aria-hidden="true"></i>
+      Artists
+     </li>
+			<li>
+				<i class="fa fa-podcast fa-fw" aria-hidden="true"></i>
+				Podcasts
+			</li>
+			<li>
+				<i class="fa fa-cog fa-fw" aria-hidden="true"></i>
+				Settings
+			</li>
+    </ul>
+   </div>
+	 <div id="bluetooth-devices" class="menu">
+			<span class="close-btn"><span>&times;</span> Close</span>
+			<h3>Devices nearby</h3>
+			<ul>
+				<li class="connected">
+					<img id="headphones-icon" class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/headphone.svg" alt="">
+					<p>
+						Major II Bluetooth
+						<span>Connected</span>
+					</p>
+				</li>
+				<li>
+					<img id="headphones-icon" class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/loudspeaker.svg" alt="">
+					<p>
+						Sonos One
+						<span>Connected</span>
+					</p>
+				</li>
+				<li>
+					<img id="headphones-icon" class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/car.svg" alt="">
+					<p>
+						Kia Motors
+						<span>Connected</span>
+					</p>
+				</li>
+			</ul>
+			<p class="info-text">
+				Make sure your bluetooth device is turned on and in range.
+			</p>
+		</div>
+	 <div id="song-options" class="menu">
+		 <span class="close-btn"><span>&times;</span> Close</span>
+		 <div id="song-info">
+		 	<img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/308622/rockstar-album-cover.jpg" alt="">
+			 <div class="artist-wrap">
+			 	<p>
+			 		<span class="title">rockstar</span>
+					<span class="artist">Post Malone, 21 Savage</span>
+			 	</p>
+			 </div>
+		 </div>
+	 	<ul>
+	 		<li>
+        <i class="fa fa-music fa-fw add" aria-hidden="true"></i>
+				Add to playlist
+			</li>
+			<li>
+				<i class="fa fa-microphone fa-fw" aria-hidden="true"></i>
+				View Artist
+			</li>
+     <li>
+      <img id="album-icon" class="svg menu-icons" src="https://emilcarlsson.se/assets/svg/compact-disc.svg" alt="">
+      View Album
+     </li>
+			<li>
+				<i class="fa fa-share-square-o fa-fw" aria-hidden="true"></i>
+				Share
+			</li>
+	 	</ul>
+	 </div>
+	 <div id="home-screen">
+		<div class="home-content">
+			<h2>Music Player App</h2>
+			<p id="made-by">Made by <a href="https://codepen.io/emilcarlsson">@emilcarlsson</a></p>
+			<div class="app-icon">
+				<img class="svg" src="https://emilcarlsson.se/assets/svg/music-note.svg" alt="">
+			</div>
+			<p id="icons-by">Icons by <a href="http://fontawesome.io/" title="Font Awesome">Font Awesome</a>, <a href="https://www.flaticon.com/authors/those-icons" title="Those Icons">Those Icons</a>,<br /><a href="https://www.flaticon.com/authors/epiccoders" title="EpicCoders">EpicCoders</a> & <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a></p>
+		</div>
+	 </div>
+  </div>
+  <div id="home-btn"></div>
+ </div>
+</div>
     </main>
   )
 }
